7efdbf83b3da4cd46c1d4b66b20ec2497fe99b94
Siddharth Singh <simsid66@gmail.com>
2021-04-26 17:20:03 +0530

    first working version, no headers

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const utils = require('./utils')
+const request = require('request');
+const fs = require('fs');
+
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.use(express.static("public"))
 
@@ -8,6 +12,35 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.get('/dl', (req, res) => {
+    let queryParams = utils.getUrlQueryParams(req.url);
+    if (queryParams == null) {
+        res.status = 400;
+        res.send("bad request");
+        return;
+    }
+    if ("id" in queryParams && "sheetName" in queryParams && "format" in queryParams) {
+        res.status = 200;
+        let id = queryParams.id;
+        let dlFormat = encodeURI(queryParams.format || "tsv");
+        let encodedSheetName = encodeURI(queryParams.sheetName);
+        let getUrl = `https://docs.google.com/spreadsheets/d/${id}/export?gid=0&format=${dlFormat}&sheet=${encodedSheetName}`;
+        request.get(getUrl, function(err, resp, body) {
+            if (!err && resp.statusCode == 200) {
+                let data = body;
+                res.send({
+                    "status": "ok",
+                    "text": body
+                });
+            }
+        });
+    } else {
+        res.status = 400;
+        res.send("bad request");
+        return;
+    }
+});
+
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`App listening at http://localhost:${port}`)
 })
---
+++ utils.js
@@ -0,0 +1,17 @@
+function getUrlQueryParams(str) {
+    let queryString = str.split("?")[1];
+    if (!queryString) return null;
+    let queryPortions = queryString.split("&");
+    let ret = {}
+
+    for (let x of queryPortions) {
+        let split = x.split("=");
+        ret[split[0]] = split[1];
+    }
+
+    return ret;
+}
+
+module.exports = {
+    getUrlQueryParams
+}
