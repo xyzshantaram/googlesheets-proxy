545f5d217ab31a08d33ab75e99ba2d57464f8153
Siddharth Singh <simsid66@gmail.com>
2021-04-26 19:21:54 +0530

    Add CORS headers

--- main.js
+++ main.js
@@ -2,11 +2,18 @@ const express = require('express')
 const utils = require('./utils')
 const request = require('request');
 const fs = require('fs');
+const cors = require('cors');
 
 const app = express()
 const port = process.env.PORT || 3000;
 
 app.use(express.static("public"))
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
