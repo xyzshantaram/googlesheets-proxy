da3487487cc04fd628beb69225f0d717a9331bd0
Siddharth Singh <simsid66@gmail.com>
2021-04-26 20:09:26 +0530

    Better error handling

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const utils = require('./utils')
 const request = require('request');
-const fs = require('fs');
-const cors = require('cors');
 
 const app = express()
 const port = process.env.PORT || 3000;
@@ -23,7 +21,7 @@ app.get('/dl', (req, res) => {
     let queryParams = utils.getUrlQueryParams(req.url);
     if (queryParams == null) {
         res.status = 400;
-        res.send("bad request");
+        res.send("bad request: no arguments supplied");
         return;
     }
     if ("id" in queryParams && "sheetName" in queryParams && "format" in queryParams) {
@@ -36,14 +34,34 @@ app.get('/dl', (req, res) => {
             if (!err && resp.statusCode == 200) {
                 let data = body;
                 res.send({
-                    "status": "ok",
+                    "status": "OK",
                     "text": body
+                });
+            } else {
+                res.send({
+                    "status": "DownloadFailed",
+                    "text": null
                 });
             }
         });
     } else {
+        let list = []
+        if (!("id" in queryParams)) {
+            list.push("id");
+        }
+        if (!("sheetName") in queryParams) {
+            list.push("sheetName");
+        }
+        if (!("format") in queryParams) {
+            list.push("format");
+        }
         res.status = 400;
-        res.send("bad request");
+        res.send({
+            status: "QueryStringMissingParams",
+            text: null,
+            missing: list.toString()
+
+        });
         return;
     }
 });
--- utils.js
+++ utils.js
@@ -14,4 +14,4 @@ function getUrlQueryParams(str) {
 
 module.exports = {
     getUrlQueryParams
-}
+};
